92239139c175b145a5f86a759ca07874037846d8
Zhaofeng Li <hello@zhaofeng.li>
2021-11-24 15:10:08 -0800

    templates/rust: Bump nightly version

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -10,7 +10,7 @@
     supportedSystems = [ "x86_64-linux" ];
 
     # Rust nightly version.
-    nightlyVersion = "2021-08-01";
+    nightlyVersion = "2021-11-20";
   in mars-std.lib.eachSystem supportedSystems (system: let
     pkgs = mars-std.legacyPackages.${system};
 
